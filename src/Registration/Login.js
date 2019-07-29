f06b8d603f826222d8507cfeaffd7c635bb51805
Robert <blaze1@gmx.net>
2019-07-30 02:18:56 +0700

    added func FE Login.js

--- src/Registration/Login.js
+++ src/Registration/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import './Login.css'
+import axios from 'axios'
 // import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 // import { MDBInput } from 'mdbreact'
 // import TextField from '@material-ui/core/TextField';
@@ -16,53 +17,73 @@ class Login extends Component {
 
 
   //Functions
-  chngeEmail = (e) => {
-    this.setState({email: e.target.valiue})
+  changeEmail = (e) => {
+    this.setState({email: e.target.value})
   }
 
-  chngePassword = (e) => {
-    this.setState({password: e.target.valiue})
+  changePassword = (e) => {
+    this.setState({password: e.target.value})
   }
 
+  login = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:5000/api/login', this.state).then( (res) => {
+      if (!res.data.token) {
+        this.setState({
+          error: res.data
+        })
+      } else {
+        this.setState({
+          error: ''
+        })
+        localStorage.setItem('token', res.data.token)
+        this.props.auth()
+      }
+    }).catch( (err) => {
+      console.log('Login_Err', err)
+    })
+  }
 
 
   //Render
   render() {
     return (
       <div className="login">
-      <div className="login_card">
-  <div className="logo">
-    <div className="logo_logo"></div>
-    <div className="logo_brand">
-    <span className="logo_header">Ellbow Dojo</span> <br />
-      No pain - no Muay Thai
+    	<div className="login_card">
+    		<form className="login_form" onSubmit={(e) => this.login()}>
+    			<div className="logo">
+    				<div className="logo_logo"></div>
+    				<div className="logo_brand">
+    					<span className="logo_header">Ellbow Dojo</span> <br />
+    					No pain - no Muay Thai
+    				</div>
+    			</div>
+    			<div>
+
+    				<div className="login_login_box">
+    					<input className="inputs" placeholder="Your Email" type="text" value={this.state.email} onChange={(e)=> this.changeEmail(e)} />
+    					<input className="inputs" placeholder="Your Password" type="password" value={this.state.password} onChange={(e)=> this.changePassword(e)} />
+    				</div>
+
+    				<div className="login_forgot">
+    					<p className="login_p">Forrgot your password?</p>
+    				</div>
+    			</div>
+
+
+    			<button type="submit" className="login_button">Sign in</button>
+
+    			<div className="login_dont_have_account">
+    				<p className="login_p">Don't have an account? <a href="/Signup">Sign up!</a></p>
+    			</div>
+    		</form>
+    	</div>
+
+    	<div className="login_txt">
+    		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et architecto, accusamus maxime! Iste beatae dicta, ipsa nulla, quos porro quis asperiores dolorum hic laborum aperiam explicabo alias, enim tempora et?
+    	</div>
+
     </div>
-  </div>
-
-<div>
-  <div className="login_login_box">
-    <input className="inputs" placeholder="Your Email" type="text" />
-    <input className="inputs" placeholder="Your Password" type="password" />
-  </div>
-
-  <div className="login_forgot">
-    <p className="login_p">Forrgot your password?</p>
-  </div>
-  </div>
-
-
-    <button type="submit" className="login_button">Sign in</button>
-
-    <div className="login_dont_have_account">
-      <p className="login_p">Don't have an account? <a href="/Signup">Sign up!</a></p>
-    </div>
-</div>
-
-<div className="login_txt">
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et architecto, accusamus maxime! Iste beatae dicta, ipsa nulla, quos porro quis asperiores dolorum hic laborum aperiam explicabo alias, enim tempora et?
-</div>
-
-      </div>
     )
   }
 
